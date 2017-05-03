4c49989a5e2667b92a721148b5de592794522ef2
Zhiwei Chen <itvzc@bernoulli>
2017-05-03 11:46:48 +0200

    delate calculated commands lines

--- bash.sh
+++ bash.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 STR="A parallel finish"
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/25-50/cluster-id4 -o ../Results/25-50/cluster-id4-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/25-50/cluster-id11 -o ../Results/25-50/cluster-id11-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/25-50/cluster-id43 -o ../Results/25-50/cluster-id43-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/25-50/cluster-id69 -o ../Results/25-50/cluster-id69-result.dat 
-echo $STR
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/25-50/cluster-id99 -o ../Results/25-50/cluster-id99-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/25-50/cluster-id129 -o ../Results/25-50/cluster-id129-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/25-50/cluster-id175 -o ../Results/25-50/cluster-id175-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/25-50/cluster-id219 -o ../Results/25-50/cluster-id219-result.dat 
-echo $STR
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/25-50/cluster-id310 -o ../Results/25-50/cluster-id310-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/25-50/cluster-id316 -o ../Results/25-50/cluster-id316-result.dat 
-echo $STR
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/50-75/cluster-id24 -o ../Results/50-75/cluster-id24-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/50-75/cluster-id25 -o ../Results/50-75/cluster-id25-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/50-75/cluster-id28 -o ../Results/50-75/cluster-id28-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/50-75/cluster-id37 -o ../Results/50-75/cluster-id37-result.dat 
-echo $STR
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/50-75/cluster-id71 -o ../Results/50-75/cluster-id71-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/50-75/cluster-id82 -o ../Results/50-75/cluster-id82-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/50-75/cluster-id128 -o ../Results/50-75/cluster-id128-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/50-75/cluster-id135 -o ../Results/50-75/cluster-id135-result.dat 
-echo $STR
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/50-75/cluster-id145 -o ../Results/50-75/cluster-id145-result.dat &
-python interaction-com-com-add.py -i ../data/20170424/Selected-data/50-75/cluster-id244 -o ../Results/50-75/cluster-id244-result.dat 
-echo $STR
 python interaction-com-com-add.py -i ../data/20170424/Selected-data/75-100/cluster-id13 -o ../Results/75-100/cluster-id13-result.dat &
 python interaction-com-com-add.py -i ../data/20170424/Selected-data/75-100/cluster-id14 -o ../Results/75-100/cluster-id14-result.dat &
 python interaction-com-com-add.py -i ../data/20170424/Selected-data/75-100/cluster-id18 -o ../Results/75-100/cluster-id18-result.dat &
